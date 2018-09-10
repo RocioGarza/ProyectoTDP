4bb42fff6ec46ddf453d701e558a8923a19bc14a
Rochi <rociogarza4e@gmail.com>
2018-09-10 08:56:15 -0300

    Generador de niveles random. Archivo nivel 0 con un nivel random generado por el Tester para ver la densidad de los enemigos

---
+++ ProyectoTDP/src/logica/GeneradorDeNiveles.java
@@ -0,0 +1,89 @@
+package logica;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+
+public class GeneradorDeNiveles{
+	
+	private File txt;
+	
+	public GeneradorDeNiveles()
+	{
+		String dir = this.getClass()+"nivel0.txt"; //El nivel 0 va estar asociado al nivel random, los demas niveles corresponden con su dificultadx
+        txt = new File(dir);
+        this.generarNivel();
+	}
+	
+	public File getNivel()
+	{
+		return txt;
+	}
+	
+	private void generarNivel()
+	{
+		Random rnd = new Random(); //Le damos la hora del sistema como semilla??
+		int n = 0;
+		try {
+			BufferedWriter arch = new BufferedWriter(new FileWriter("nivel0.txt"));
+			for(int j=0; j<1000; j++)
+			{	
+			for(int i=0; i<1000; i++)
+			{
+				n = rnd.nextInt(400);
+				switch(n) 
+				{
+					case 0: case 1: case 2: case 3: case 4: case 5:
+						arch.write("A"); // 1,5%
+						break;
+					case 6: case 7: case 8: case 9:
+						arch.write("B"); // 1%
+						break;
+					case 10: case 11: case 12: case 13: 
+						arch.write("C"); // 1%
+						break;
+					case 14: case 15: case 16: case 17: case 18: case 19:
+						arch.write("D"); // 1,5%
+						break;
+					case 20: case 21: case 22: case 23:
+						arch.write("E"); // 1%
+						break;
+					case 24: 
+						arch.write("1"); // 0,25%
+						break;
+					case 25: case 26: case 27: case 28:
+						arch.write("2"); // 1%
+						break;					
+					case 29: case 30: case 31:
+						arch.write("3"); // 0,75%
+						break;
+					case 32: case 33:
+						arch.write("4"); // 0,5%
+						break;
+					case 34:
+						arch.write("5"); // 0,25%
+						break;
+					default: 
+						arch.write("."); // 100-8,75=91,25%
+						break;	
+				}
+			}
+			arch.write('\n');
+		}			
+			arch.close();
+			
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace(); //Como manejamos esta excepcion?
+		}
+		
+		
+		
+		
+	}
+	
+}
---
+++ ProyectoTDP/src/logica/testerGradorNiv.java
@@ -0,0 +1,9 @@
+package logica;
+
+public class testerGradorNiv {
+	public static void main (String args[])
+	{
+		GeneradorDeNiveles test = new GeneradorDeNiveles();
+	}
+
+}
