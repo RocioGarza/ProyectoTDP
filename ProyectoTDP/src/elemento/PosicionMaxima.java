dd41623371ea8ee766a509bf4d68f8bd0a010da8
Manuel Enriquez Merino <manuel_em7@hotmail.com>
2018-09-13 12:40:34 -0300

    Actualizado el uml con metodos

--- ProyectoTDP/src/elemento/PosicionMaxima.java
+++ ProyectoTDP/src/elemento/PosicionMaxima.java
@@ -6,21 +6,11 @@ public class PosicionMaxima {
 	private static final int Y = 1280;
 	private static PosicionMaxima instancia;
 	
-	public static PosicionMaxima getPosicionMaxima() {
-		if(instancia==null)
-			instancia = new PosicionMaxima();
-		return instancia;
-	}
-	
 	public static int getX() {
-		if(instancia==null)
-			instancia = new PosicionMaxima();
 		return X;
 	}
 	
 	public static int getY() {
-		if(instancia==null)
-			instancia = new PosicionMaxima();
 		return Y;
 	}
 
