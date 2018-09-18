a4237ee0876749eb52b67a22a8ab66bf64e3769d
Manuel Enriquez Merino <manuel_em7@hotmail.com>
2018-09-18 12:04:27 -0300

    Diagramas de interaccion de disparo de enemigo y cambio en thread de movimiento

--- ProyectoTDP/src/enemigo/Enemigo.java
+++ ProyectoTDP/src/enemigo/Enemigo.java
@@ -9,15 +9,21 @@ public abstract class Enemigo extends Personaje {
 	
 	public class Hilo extends Thread{
 		public void run(){
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			//mover el objeto de una pared a la otra sin parar
 			while(true){
 
 				//avanza a la derecha hasta chocar con la pared
-				while(getPosicion().getX() < PosicionMaxima.getX()){
+				while(getPosicion().getX() < PosicionMaxima.getX()-getPosicion().getAncho()-1){
 					getPosicion().moverX( 1 );
 					getGrafico().mover('d');
 					try {
-						Thread.sleep(100);
+						Thread.sleep(10);
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -26,11 +32,11 @@ public abstract class Enemigo extends Personaje {
 				}
 
 				//avanza a la izquierda hasta chocar con la pared
-				while(getPosicion().getX() > 0){
+				while(getPosicion().getX() > getPosicion().getAncho()+1){
 					getPosicion().moverX( -1 );
 					getGrafico().mover('a');
 					try {
-						Thread.sleep(100);
+						Thread.sleep(10);
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
